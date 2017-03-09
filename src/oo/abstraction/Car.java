149259f56af039c9a8a76006f5184a5e39b895ed
psli0101 <p.s.li19980101@gmail.com>
2017-03-09 19:39:28 +0800

    Commit and push

---
+++ src/oo/abstraction/Car.java
@@ -0,0 +1,15 @@
+package oo.abstraction;
+
+public class Car {
+	String brand;
+	String name;
+	String type;
+	int cc;
+	
+	public Car(String brand, String name, String type, int cc){
+		this.brand = brand;
+		this.name = name;
+		this.type = type;
+		this.cc = cc;
+	}
+}
---
+++ src/oo/abstraction/CarTester.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class CarTester {
+
+	public static void main(String[] args) {
+		Car car1 = new Car("Toyota", "Altis", "Sedan", 1800);
+	}
+
+}
