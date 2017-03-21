0aeaba9ea392ad23de40dc14b48abe4d9e6df648
psli0101 <p.s.li19980101@gmail.com>
2017-03-21 16:22:27 +0800

    create arrays

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -6,6 +6,25 @@ public class CarTester {
 		Car car1 = new Car("Toyota", "Altis", 1800);
 		Car car2 = new Car("Honda", "Accord", 2000);
 		Car car3 = new Car("Toyota", "Wish", 2000);
+
+		Car[] cars = new Car[3];
+		cars[0] = new Car("Toyota", "Altis", 1800);
+		cars[1] = new Car("Honda", "Accord", 2000);
+		cars[2] = new Car("Toyota", "Wish", 2000);
+		System.out.println(cars[1].name);
+		int i = 0;
+		for (i = 0; i < 3; i++) {
+			System.out.println(cars[i].name);
+		}
+		System.out.println(i);
+		for (i = 0; i <= 9; i = i + 2) {
+			System.out.println(i);
+		}
+		System.out.println(i);
+		class AA {
+
+		}
+		AA aa = new AA();
 	}
 
 }
