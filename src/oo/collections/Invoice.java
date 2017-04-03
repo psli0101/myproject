2ac4c07d9c2469ff911db1a825db271cffb3a20c
P.S. Li <p.s.li19980101@gmail.com>
2017-04-03 12:45:14 +0800

    finish homework - Invoice.java

--- src/oo/collections/Invoice.java
+++ src/oo/collections/Invoice.java
@@ -7,25 +7,25 @@ public class Invoice {
 	 * 題目： 本次開獎號為331, 821, 886, 554 讓使用者輸入發票碼 (長度不限) 判斷尾數3位是否中獎
 	 */
 	public static void main(String[] args) {
-		boolean flag;
+		boolean flag = false;
 		ArrayList<String> num = new ArrayList();
 		num.add("331");
 		num.add("821");
 		num.add("886");
 		num.add("554");
+		System.out.print("請輸入發票號碼：");
 		Scanner inPut = new Scanner(System.in);
 		String n = inPut.nextLine();
 		String a = n.substring(n.length() - 3);
 		for (int i = 0; i < 4; i++) {
-			if(a.equals(num.get(i))){
+			if (a.equals(num.get(i))) {
 				flag = true;
 			}
 		}
-		if(flag = true){
+		if (flag) {
 			System.out.println("有中獎");
-		}else{
+		} else {
 			System.out.println("沒中獎");
 		}
 	}
-
 }
